f1601b76b7ba48833fdf689bc805deb63bd831c5
kuriimu01 <114774348+kuriimu01@users.noreply.github.com>
2024-01-04 01:15:34 +0200

    Display Digits

--- code/Main.java
+++ code/Main.java
@@ -1,13 +1,13 @@
 import java.lang.*;
+import java.util.Scanner;
 
 class Main{
-    public static void main(String[] args){
-        switch (case_num){
-            case 1:
-                System.out.println("1");
-                break;
-            case 2:
-                System.out.println("2");
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int j = s.nextInt();
+        String jStr = Integer.toString(j);
+        for (int i=1; i<=jStr.length(); i++){
+            System.out.println("№"+i+"  digit is: "+jStr.charAt(i-1));
         }
     }
 }
