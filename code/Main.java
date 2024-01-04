bab0c7ee7a82f55bad5c4de0224ae9ebfaa9091e
kuriimu01 <114774348+kuriimu01@users.noreply.github.com>
2024-01-04 16:20:46 +0200

    Multiplication table (Nested loops)

--- code/Main.java
+++ code/Main.java
@@ -1,45 +1,12 @@
 import java.lang.*;
-import java.util.Scanner;
 
 class Main{
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int j = s.nextInt();
-        String jStr = Integer.toString(j);
-        for (int i=1; i<=jStr.length(); i++){
-            int num = jStr.charAt(i-1)-'0';
-            switch (num){
-                case 0:
-                    System.out.print("zero ");
-                    break;
-                case 1:
-                    System.out.print("one ");
-                    break;
-                case 2:
-                    System.out.print("two ");
-                    break;
-                case 3:
-                    System.out.print("three ");
-                    break;
-                case 4:
-                    System.out.print("four ");
-                    break;
-                case 5:
-                    System.out.print("five ");
-                    break;
-                case 6:
-                    System.out.print("six ");
-                    break;
-                case 7:
-                    System.out.print("seven ");
-                    break;
-                case 8:
-                    System.out.print("eight ");
-                    break;
-                case 9:
-                    System.out.print("nine ");
-                    break;
+        for (int i = 1; i<=9; i++){
+            for (int j = 1; j<=9; j++){
+                System.out.println(i +"*"+j+"= " + (i*j));
             }
+            System.out.print("\n");
         }
     }
 }
