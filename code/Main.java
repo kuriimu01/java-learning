aa1c9706ba78b55e5fea428c0af307bf91736c5f
kuriimu01 <114774348+kuriimu01@users.noreply.github.com>
2024-01-04 15:11:43 +0200

    Display a number in words

--- code/Main.java
+++ code/Main.java
@@ -5,27 +5,41 @@ class Main{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int j = s.nextInt();
-        switch (num){
-            case 0: System.out.println("zero");
+        String jStr = Integer.toString(j);
+        for (int i=1; i<=jStr.length(); i++){
+            int num = jStr.charAt(i-1)-'0';
+            switch (num){
+                case 0:
+                    System.out.print("zero ");
                     break;
-            case 1: System.out.println("one");
-                break;
-            case 2: System.out.println("two");
-                break;
-            case 3: System.out.println("three");
-                break;
-            case 4: System.out.println("four ");
-                break;
-            case 5: System.out.println("five");
-                break;
-            case 6: System.out.println("six");
-                break;
-            case 7: System.out.println("seven");
-                break;
-            case 8: System.out.println("eight");
-                break;
-            case 9: System.out.println("nine");
-                break;
+                case 1:
+                    System.out.print("one ");
+                    break;
+                case 2:
+                    System.out.print("two ");
+                    break;
+                case 3:
+                    System.out.print("three ");
+                    break;
+                case 4:
+                    System.out.print("four ");
+                    break;
+                case 5:
+                    System.out.print("five ");
+                    break;
+                case 6:
+                    System.out.print("six ");
+                    break;
+                case 7:
+                    System.out.print("seven ");
+                    break;
+                case 8:
+                    System.out.print("eight ");
+                    break;
+                case 9:
+                    System.out.print("nine ");
+                    break;
+            }
         }
     }
 }
