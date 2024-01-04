7054f1ac7f4b17910b452e90ae99e97663b3b7a0
kuriimu01 <114774348+kuriimu01@users.noreply.github.com>
2024-01-04 14:50:58 +0200

    vault backup: 2024-01-04 14:50:58

--- code/Main.java
+++ code/Main.java
@@ -5,9 +5,27 @@ class Main{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int j = s.nextInt();
-        String jStr = Integer.toString(j);
-        for (int i=1; i<=jStr.length(); i++){
-            System.out.println("№"+i+"  digit is: "+jStr.charAt(i-1));
+        switch (num){
+            case 0: System.out.println("zero");
+                    break;
+            case 1: System.out.println("one");
+                break;
+            case 2: System.out.println("two");
+                break;
+            case 3: System.out.println("three");
+                break;
+            case 4: System.out.println("four ");
+                break;
+            case 5: System.out.println("five");
+                break;
+            case 6: System.out.println("six");
+                break;
+            case 7: System.out.println("seven");
+                break;
+            case 8: System.out.println("eight");
+                break;
+            case 9: System.out.println("nine");
+                break;
         }
     }
 }
