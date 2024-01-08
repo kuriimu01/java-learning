5d3a570143fdfac40268de9f505231a79c0c6a89
kuriimu01 <114774348+kuriimu01@users.noreply.github.com>
2024-01-08 23:09:22 +0200

    Random array

--- code/Main.java
+++ code/Main.java
@@ -1,20 +1,13 @@
-    public static void main(String[] args) {
-        int[] arr = {11, 2, 32, 1, 23, 44, 6};
-        boolean unsorted = true;
-        int temp;
-        while (unsorted){
-            unsorted=false;
-            for (int i = 0; i<arr.length-1;i++) {
-                if (arr[i]>arr[i+1]){
-                    temp=arr[i];
-                    arr[i]=arr[i+1];
-                    arr[i+1]=temp;
-                    unsorted=true;
-                }
-            }
-        }
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] arg) {
+        Random rd = new Random();
+        int[] arr = new int[10];
         for (int i: arr){
-            System.out.print(i+" ");
+            arr[i]= rd.nextInt(1,100);
+            System.out.print(arr[i]+"  ");
         }
     }
 }
