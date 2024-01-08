932b8ef698d0bff658d9c1e9d1c4dc641a91290c
kuriimu01 <114774348+kuriimu01@users.noreply.github.com>
2024-01-08 21:33:45 +0200

    Bubble sort with recursion

--- code/Main.java
+++ code/Main.java
@@ -1,7 +1,23 @@
-import java.lang.*;
+import java.util.Arrays;
 
-class Main{
-    public static String main(String str) {
-        return StringBuilder(str).reverse();
+public class BubbleSort {
+    static void bubbleSort(int[] arrUnsort) {
+        int count = 0;
+        for (int i = 0; i < arrUnsort.length - 1; i++)
+            if (arrUnsort[i] > arrUnsort[i + 1]) {
+                int temp = arrUnsort[i];
+                arrUnsort[i] = arrUnsort[i + 1];
+                arrUnsort[i + 1] = temp;
+                count++;
+            }
+        if (count > 0) {
+            bubbleSort(arrUnsort);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] myArr = {104, 64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(myArr);
+        System.out.println(Arrays.toString(myArr));
     }
 }
