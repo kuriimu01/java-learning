ad691b167e4cce3bfa77b179e99319c95a92851c
kuriimu01 <114774348+kuriimu01@users.noreply.github.com>
2024-01-09 15:45:26 +0200

    Reverse Random Array

--- code/Main.java
+++ code/Main.java
@@ -5,9 +5,20 @@ public class Main {
     public static void main(String[] arg) {
         Random rd = new Random();
         int[] arr = new int[10];
-        for (int i: arr){
-            arr[i]= rd.nextInt(1,100);
-            System.out.print(arr[i]+"  ");
+        for (int k = 0; k < arr.length; k++) {
+            arr[k] = rd.nextInt(101);
+            System.out.print(arr[k] + "  ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+
+        for (int c : arr) {
+            System.out.print(c + "  ");
         }
     }
 }
