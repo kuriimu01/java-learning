9b4b5f6f59166eb147555539fe8fd17e82e8393e
kuriimu01 <114774348+kuriimu01@users.noreply.github.com>
2024-01-13 23:31:16 +0200

    Convert a Boolean to a String Kata

--- code/Main.java
+++ code/Main.java
@@ -1,24 +1,12 @@
 public class Main {
 
     public static void main(String[] arg) {
-        Boolean[] arrayOfSheeps = {true,  true,  true,  false,
-                true,  true,  true,  true ,
-                true,  false, true,  false,
-                true,  false, false, true ,
-                true,  true,  null,  true ,
-                false, false, true,  true };
-        Counter.countSheeps(arrayOfSheeps);
+        boolean b = true;
+        System.out.println(BooleanToString.convert(b));
     }
 }
-class Counter {
-    public static int countSheeps(Boolean[] arrayOfSheeps) {
-        int count = 0;
-        for (Boolean i : arrayOfSheeps) {
-            if (i != null && i) {
-                count++;
-            }
-        }
-        System.out.print(count);
-        return count;
+class BooleanToString {
+    public static String convert(boolean b) {
+        return Boolean.toString(b);
     }
 }
