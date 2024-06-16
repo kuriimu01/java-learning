bed2dacb8a5118ec0ed37fc35f6d6488852cc9b7
kuriimu01 <114774348+kuriimu01@users.noreply.github.com>
2024-06-17 00:04:46 +0300

    Stream i/o file copy. Student challenge

--- code/Main.java
+++ code/Main.java
@@ -1,12 +1,19 @@
-public class Main {
+import java.io.*;
 
-    public static void main(String[] arg) {
-        boolean b = true;
-        System.out.println(BooleanToString.convert(b));
-    }
-}
-class BooleanToString {
-    public static String convert(boolean b) {
-        return Boolean.toString(b);
+public class Main {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("C:/Users/Користувач/Desktop/source1.txt");
+            FileOutputStream fos = new FileOutputStream("C:/Users/Користувач/Desktop/source2.txt");
+            int x;
+            String str = "";
+            do{x=fis.read();if (x!=-1) {
+                str+=String.valueOf((char)x);
+            }
+            }while(x!=-1);
+            fos.write(str.getBytes());
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
