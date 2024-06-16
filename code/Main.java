fe7a4624c1fdb769c81457c489824151aad304b3
kuriimu01 <114774348+kuriimu01@users.noreply.github.com>
2024-06-17 00:19:43 +0300

    Concat two files student challenge

--- code/Main.java
+++ code/Main.java
@@ -1,19 +1,20 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            FileInputStream fis = new FileInputStream("C:/Users/Користувач/Desktop/source1.txt");
-            FileOutputStream fos = new FileOutputStream("C:/Users/Користувач/Desktop/source2.txt");
-            int x;
-            String str = "";
-            do{x=fis.read();if (x!=-1) {
-                str+=String.valueOf((char)x);
-            }
-            }while(x!=-1);
-            fos.write(str.getBytes());
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
+    public static void main(String[] args) throws Exception {
+        FileInputStream fis1 = new FileInputStream("C:/Users/Користувач/Desktop/source1.txt");
+        FileInputStream fis2 = new FileInputStream("C:/Users/Користувач/Desktop/source2.txt");
+
+        FileOutputStream fos = new FileOutputStream("C:/Users/Користувач/Desktop/glued.txt");
+        SequenceInputStream sis = new SequenceInputStream(fis1,fis2);
+        int x;
+        while ((x=sis.read())!=-1){
+            fos.write(x);
         }
+        sis.close();
+        fis1.close();
+        fis2.close();
+        fos.close();
+
     }
 }
